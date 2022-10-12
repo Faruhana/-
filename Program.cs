1b8f6a4e795a0adbf0611aa44f090fe1f80f64c3
Faruhana <jordon.thomas@mail.ru>
2022-10-12 12:45:52 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет, это моя 1-ая программа на С#");
+            //Здесь я создам однострочный комментарий.
+            /*Здесь я создам многостройчный комментарий:
+             Следующая строка будет отвечать за то,
+             что бы компилятор не закрылся сразу, 
+             а только после ввода какого-либо символа с клавиатуры.
+            */
+            Console.ReadKey();
+        }
+    }
+}
